e9d5e921454b8cbabc6021f1030e4598b71bfc07
Nicolas_Wadoux <nicolas.wadoux@yahoo.fr>
2021-12-30 20:27:53 +0100

    and popUpTo on back navigations and change header title (fix weird bug)

--- app/src/main/java/com/example/spacedim/MainActivity.kt
+++ app/src/main/java/com/example/spacedim/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.spacedim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navCtrl = this.findNavController(R.id.mainNavHostFrag)
+        // android:label in navigation.xml not working, fix by this code
+        navCtrl.addOnDestinationChangedListener{
+            ctrl, dest, args -> title = dest.label
+        }
+        NavigationUI.setupActionBarWithNavController(this, navCtrl)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navCtrl = this.findNavController(R.id.mainNavHostFrag)
+        return navCtrl.navigateUp()
     }
 }
